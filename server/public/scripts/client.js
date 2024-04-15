6137e3172cbcfe12181fa4a203ec94a1756bd670
Heather Haymond <hheatherjean11@gmail.com>
2024-04-15 01:20:01 -0500

    get and post... I didn't stop my server to commit

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,77 @@
-console.log('JS is sourced!');
+console.log("JS is sourced!");
+
+//GET -> Read
+function toDo() {
+  // axios call to server
+  axios({
+    method: "GET",
+    url: "/toDo",
+  })
+    .then((response) => {
+      console.log("GET /toDo response.data", response.data);
+      renderToDo(response.data);
+    })
+    .catch((error) => {
+      console.log("Error", error);
+    });
+} // end toDo
+
+
+//Rendering Function
+function renderToDo(toDoList) {
+  let toDoList = response.data;
+  let locateTableBody = document.getElementById("toDoTable");
+  locateTableBody.innerHTML = "";
+
+  for (let toDo of toDoList) { //Loop over each item and append data to the DOM
+    if (toDo.complete === true) {
+      toDoTableBody.innerHTML += `
+                <tr id="completed" data-testid="toDoItem">
+                  <td>${toDo.text}</td>
+                  <td>${toDo.completed}</td>
+                    <button onclick="completeTodo(${toDo.id})" data-testid="completeButton">Completed</button>
+                    <button onclick="deleteTodo(${toDo.id})" data-testid="deleteButton">Delete</button>
+                  </td>
+                </tr>
+              `;
+    } else {
+      toDoTableBody.innerHTML += `
+                <tr data-testid="toDoItem">
+                <td>${toDo.text}</td>
+                <td><button onclick="completeTodo(${toDo.id})" data-testid="completeButton">Completed</button></td>
+                <td><button onclick="deleteToDo(${toDo.id})" data-testid="deleteButton">Delete</button></td>
+            </tr>
+                `;
+    }
+  }
+}
+
+// POST ->Create
+function addToDo(){
+let toDoText= document.getElementById("Input").value
+console.log('todoText:', toDoText)
+axios({
+    method: 'POST',
+    url: '/todos',
+    data: {text: toDoText}
+}).then((response) => {
+    toDoText= ''; 
+    fetchAndRenderToDo(); 
+}).catch((error) => {
+    console.log('Error in POST route', error);
+}); 
+}
+// PUT -> Update/Edit
+// DELETE -> Delete
+
+
+// 3. Each rendered to-do item must have:
+// * `data-testid="toDoItem"`
+// 4. Each to-do item's "delete" button must have:
+// * `data-testid="deleteButton"`
+// * This button must be a child of the element that has the `data-testid="toDoItem"` on it.
+// 5. Each to-do item's "mark complete" button must have:
+// * `data-testid="completeButton"`
+// * This button must be a child of the element that has the `data-testid="toDoItem"` on it.
+// 6. Each completed to-do item must have:
+// * A CSS class of `completed` applied to its `data-testid="toDoItem"` element.
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,7 +1,50 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
+// GET
+router.get('/', (req, res) => {
+  console.log('GET in /toDo');
+  //sql is similar to axios. it directs info to the DB
+  const sqlText = `
+        SELECT * FROM todos
+        ORDER BY id;
+`;
+//pool
+  pool
+      .query(sqlText)
+      .then((dbResult) => {
+          let todoTask = dbResult.rows;
+          res.send(todoTask);
+      })
+      .catch((dbError) => {
+          console.log('Get error in server:', dbError);
+          res.sendStatus(500);
+      });
+});
+    // POST
+    router.post('/', (req, res) => {
+      console.log('trying to get the todos!!');
+      let todo = req.body.text;
+      console.log('req.body.text', req.body.text);
+      sqlText = `
+          INSERT INTO todos
+              (text)
+              VALUES ($1);
+      `;
+      pool.query(sqlText, [text])
+    // here, res.sendStatus is the express variable, don't overwrite with
+    //      .then variable???
+    .then((dbRes) => {
+        console.log('successful POST, inserted a row!', dbRes);
+        res.sendStatus(201);
+    })
+    .catch((dbErr) => {
+        console.log('Whoa...server error in POST for /todos:', dbErr);
+        res.sendStatus(500);
+    })
+});
 
-
+    // PUT
+    // DELETE
 
 module.exports = router;
